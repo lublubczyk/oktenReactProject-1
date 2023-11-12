19575261b6d95c2f632bd6988eee8230513b559b
lublubczyk <lubczyk@ukr.net>
2023-11-12 20:17:48 +0200

    change css

--- okten-react-project-1/src/components/Header/ChangeTheme/ChangeTheme.tsx
+++ okten-react-project-1/src/components/Header/ChangeTheme/ChangeTheme.tsx
@@ -7,16 +7,14 @@ const ChangeTheme = () => {
    
     const [flag, setFlag] = useState<boolean>(true);
     const body = document.getElementsByTagName('body')[0];
-    const elements = Array.from(document.getElementsByClassName('MoviesListCards_MoviesListCards__J3R5N'));
-    const cards = elements.filter(element => element instanceof HTMLElement) as HTMLElement[];
-
+    
     const change = () => {
         if (flag) {
             body.style.backgroundColor = 'black';
-            cards.forEach((element: HTMLElement) => element.style.backgroundColor = 'gray');
+            body.style.color = 'white';
         } else {
             body.style.backgroundColor = '';
-            cards.forEach((element: HTMLElement) => element.style.backgroundColor = '')
+            body.style.color = '';
         }
         setFlag(!flag);
     };
